02ef4e1be1d4f33705814427a179807ce42579b1
nidhi8404 <102024307+nidhi8404@users.noreply.github.com>
2023-04-04 02:04:56 +0530

    Add files via upload

---
+++ SEM-4/COA/Move Set of Numbers.asm
@@ -0,0 +1,16 @@
+;Program to move set of numbers from one memory block to another
+
+.model small
+.code
+start:
+MOV SI,2000h
+MOV DI,4000h
+MOV CL,05h
+LOOP1: MOV bl,[SI]
+MOV [DI],bl
+INC SI
+INC DI
+DEC CL
+JNZ LOOP1
+int 03h
+end start
