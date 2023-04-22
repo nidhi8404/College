728e8f66613db82d5a057c467c41d49f5a5b1f2c
nidhi8404 <102024307+nidhi8404@users.noreply.github.com>
2023-04-23 02:02:25 +0530

    add at begin,curr Delete first n last char text.

---
+++ SEM-4/PYTHON/GUI/Text widget.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+parent = tk.Tk()
+# create the widget.
+mytext = tk.Text(parent)
+# insert a string at the beginning
+mytext.insert('1.0', "- Python exercises, solution -")
+# insert a string into the current text
+mytext.insert('1.19', ' Practice,')
+# delete the first and last character (including a newline character)
+mytext.delete('1.0')
+mytext.delete('end - 2 chars')
+mytext.pack()
+parent.mainloop()
+
+#Insert a string at the beginning then insert a string into the current text. Delete the first and last character of the text.
