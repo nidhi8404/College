990eed3c804026b4b237b423791c0739df2efabd
nidhi8404 <102024307+nidhi8404@users.noreply.github.com>
2023-04-25 00:59:57 +0530

    specified dictionary data which has index labels

--- SEM-4/PYTHON/Pandas/Dataframe w index labels.py
+++ SEM-4/PYTHON/Pandas/Dataframe w index labels.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthe'],
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael','Matthe','Nidhi','i','j'],
              'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
              'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
              'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
