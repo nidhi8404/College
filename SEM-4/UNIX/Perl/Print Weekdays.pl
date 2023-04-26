a8cf737132e986f1cfc6ac41c374bcd55bb8336f
Nidhi Shinde <102024307+nidhi8404@users.noreply.github.com>
2023-04-26 18:44:57 +0530

    Program to print weekdays

---
+++ SEM-4/UNIX/Perl/Print Weekdays.pl
@@ -0,0 +1,9 @@
+@weekdays = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+print "Enter a number from 1 to 7: ";
+$number = <>;
+if ($number >= 1 && $number <= 7) {
+    print "$weekdays[$number - 1]\n";
+}
+else {
+    print "Invalid number\n";
+}
