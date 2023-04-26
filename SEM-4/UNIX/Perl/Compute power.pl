91f77a2b083da529ef7f78184b7701f0ebd56977
Nidhi Shinde <102024307+nidhi8404@users.noreply.github.com>
2023-04-26 18:41:41 +0530

    to compute power of a given number

---
+++ SEM-4/UNIX/Perl/Compute power.pl
@@ -0,0 +1,14 @@
+!/bin/perl
+print " Enter base number: ";
+$base=<>;
+print " Enter power to be calculated: ";
+$power=<>;
+$result=$base**$power;
+print " Result is : " . $result;
+
+
+# ---------------------Output:-----------------------
+# root@MUM084:~/Desktop# perl read.pl
+# Enter base number: 3
+# Enter power to be calculated: 2
+# Result is : 9
