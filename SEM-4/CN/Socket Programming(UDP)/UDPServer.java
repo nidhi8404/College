e69fae89dfe9bea63fe9a85b7b35c4646082109e
Nidhi Shinde <102024307+nidhi8404@users.noreply.github.com>
2023-04-27 22:08:19 +0530

    Update UDPServer.java

--- SEM-4/CN/Socket Programming(UDP)/UDPServer.java
+++ SEM-4/CN/Socket Programming(UDP)/UDPServer.java
@@ -7,8 +7,7 @@ DatagramSocket serverSocket= new  DatagramSocket(9876);
 byte[] receiveData= new byte[1024];
 byte[] sendData= new byte[1024];
 while(true){
-DatagramPacket receivePacket= new
-DatagramPacket(receiveData, receiveData.length);
+DatagramPacket receivePacket= new DatagramPacket(receiveData, receiveData.length);
 serverSocket.receive(receivePacket);
 String sentence= new String(receivePacket.getData());
 System.out.println("RECEIVED: "+ sentence);
